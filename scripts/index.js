e0b14ba1bbe99065332a40f14d0358ff8944d9e2
Jared Girvan <tkdinst.429897@gmail.com>
2022-10-20 20:32:25 -0400

    Update for my Project

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 const initialCards = [
-  (object1 = {
+  {
     name: "London",
     link: "https://images.unsplash.com/photo-1520967824495-b529aeba26df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-  }),
+  },
 
-  (object2 = {
+  {
     name: "Suwon",
     link: "https://images.unsplash.com/photo-1509327522225-6ce58f4795c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1461&q=80",
-  }),
+  },
 
-  (object3 = {
+  {
     name: "Japan",
     link: "https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1492&q=80",
-  }),
+  },
 
-  (object4 = {
+  {
     name: "India",
     link: "https://images.unsplash.com/photo-1524492412937-b28074a5d7da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
-  }),
+  },
 
-  (object5 = {
+  {
     name: "Vanoise National Park",
     link: "https://images.unsplash.com/photo-1500993855538-c6a99f437aa7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-  }),
+  },
 
-  (object6 = {
+  {
     name: "Bucharest",
     link: "https://images.unsplash.com/photo-1574616979112-f9f52d3747f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-  }),
+  },
 ];
