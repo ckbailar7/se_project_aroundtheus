9258bbeee07913398ea4fc5f15decb9b1d1bda09
Jared Girvan <tkdinst.429897@gmail.com>
2022-10-24 17:48:32 -0400

    Update for my Project

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,28 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];
+
+// let titleButton = document.querySelector(".profile__title-button");
+// let modal = document.querySelector(".modal");
+
+// //Hide the Modal Box
+// modal.setAttribute("style", "display: none");
+
+// function modalDisplay() {
+//   modal.setAttribute("style", "display: block");
+// }
+
+// //Event listener for clicking the button
+// titleButton.addEventListener("click", modalDisplay());
+
+const openEditModalButton = document.querySelector(".profile__title-button");
+const popUp = document.querySelector(".modal");
+const closeEditModalButton = document.querySelector(".modal-button");
+
+openEditModalButton.addEventListener("click", () => {
+  popUp.classList.add("modal__opened");
+});
+
+closeEditModalButton.addEventListener("click", () => {
+  popUp.classList.remove("modal__opened");
+});
